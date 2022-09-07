95842323bddbdb5ae88dcb42d82e975709b86df1
awllms77 <113059033+awllms77@users.noreply.github.com>
2022-09-07 16:11:48 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "840657331367841792; // your league ID
+export const leagueID = "840657331367841792" // your league ID
 export const leagueName = "Choices"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
